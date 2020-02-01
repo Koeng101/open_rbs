ba8835b8c8a42d9abce6103f07818338aa01e300
Keoni Gandall <koeng101@gmail.com>
2020-02-01 15:37:16 -0800

    Added fold function

--- rbs_calc.py
+++ rbs_calc.py
@@ -1,4 +1,5 @@
 import re
+import subprocess
 
 class RbsCalculator:
     def __init__(self): # Add more parameters
@@ -6,16 +7,20 @@ class RbsCalculator:
             "GUG": -0.0748,
             "UUG": -0.0435, 
             "CUG":-0.03406} # Taken from https://github.com/hsalis/Ribosome-Binding-Site-Calculator-v1.0/blob/master/RBS_Calculator.py 
-
-    def dG_tot(self,mRNA:str) -> float:
+        self.rRNA = 'ACCUCCUUA"'
+    
+    def fold_rna(self,RNA:str) -> float:
+        return float(subprocess.check_output('echo {} | LinearFold/./linearfold --verbose | grep -F "Energy(kcal/mol):" | cut -d ":" -f2'.format(RNA),shell=True).decode('utf-8'))
+        
+    def dG_tot(self,mRNA_pre:str,mRNA_post:str) -> float:
         # Calculate dG_mRNA_rRNA
         dG_mRNA_rRNA = 1
 
         # Calculate dG_mRNA
-        dG_mRNA = 1
+        dG_mRNA = self.fold_rna(mRNA_pre+mRNA_post)
 
         # Calculate dG_start
-        dG_start = 1
+        dG_start = self.dG_start[mRNA_post[0:3]]
 
         # Calculate dG_spacing
         dG_spacing = 1
@@ -31,4 +36,6 @@ class RbsCalculator:
         mRNA = mRNA.replace('T','U')
         if re.search(mRNA, "^[AUGC]*$") is None:
             raise ValueError('Not an mRNA sequence')
+
+
 
